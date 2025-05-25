3433216205ed87256ac4b0eeded372e7b8731df7
Ndumiso <ndumisovee@gmail.com>
2025-05-25 20:51:00 +0200

    fixed file name

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+let result = "";
+
+for (let i = 0; i < lines.length; i++) {
+  result += lines[i] + "\n";
+}
+
+console.log(result.trim());
